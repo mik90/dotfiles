e97fa4f90b7a2bd0cf7091daf450fa76aafacbd4
Mike Kaliman <kaliman.mike@gmail.com>
2022-10-14 23:01:50 -0400

    va-api support

--- framework-nixos/configuration.nix
+++ framework-nixos/configuration.nix
@@ -13,6 +13,20 @@
 
   hardware.cpu.intel.updateMicrocode = true;
   hardware.enableRedistributableFirmware = true;
+  
+  # Needed for VA-API https://nixos.wiki/wiki/Accelerated_Video_Playback
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
 
   # Use the systemd-boot EFI boot loader.
   boot.loader = {
